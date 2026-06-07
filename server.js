f3eff6b003b1c1c11d9cbd806496e6744c24204d
boldbug <boldbug9@gmail.com>
2026-06-07 17:10:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Library API");
+})
+
+const books = [];
+
+app.get("/books",(req,res)=>{
+    res.json(books);
+})
+
+app.listen(3000,()=>{
+    console.log("Server running...");
+})
+
+app.use(express.json());
+let nextId = 1;
+
+app.post("/books",(req,res)=>{
+    if (!req.body.title || !req.body.author) {
+        return res.status(400).send("Missing title or author");
+    }
+
+    const book = {
+        id : nextId++,
+        title : req.body.title,
+        author : req.body.author,
+    }
+    books.push(book);
+    res.status(201).json(book);
+
+
+})
+
+
+
+app.get("/books/:id",(req,res)=>{
+    const id = Number(req.params.id);
+
+    const book = books.find(book=>book.id===id);
+
+    if(!book){
+        return res.status(404).send("Not found");
+    }
+
+   res.json(book);
+})
+
+
+app.get("/search",(req,res)=>{
+    const bookName = req.query.title;
+
+    const book = books.find(book=>book.title===bookName);
+    if(!book){
+        return res.status(404).send("Book not found");
+    }
+    res.json(book);
+});
+
+app.delete("/books/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const index = books.findIndex(book=>book.id===id);
+
+    if(index === -1){
+        return res.status(404).send("Book not found");
+    }
+
+    books.splice(index,1);
+
+    res.status(204).send();
+})
+
+app.patch("/books/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const book = books.find(book=>book.id===id);
+
+    if(!book){
+        return res.status(404).send("Book not found");
+    }
+    if(req.body.title){
+        book.title = req.body.title;
+    }
+    if(req.body.author){
+        book.author = req.body.author;
+    }
+
+    res.json(book);
+})
