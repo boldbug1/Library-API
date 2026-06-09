45c06b7b0d950ba071b149cf5ecad35ed4df72bb
boldbug <boldbug9@gmail.com>
2026-06-09 19:22:30 +0530

    fix : update routes path in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const logger = require("./middleware/logger.js")
-const booksRouter = require("./routes/books.js")
+const booksRouter = require("./routes/booksRoutes.js")
 //Middleware
 app.use(express.json());
 app.use(express.static("public"));
