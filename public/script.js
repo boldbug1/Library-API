72bc54b039e8c7be7362c8c846ada8ecf0b7fd3e
boldbug <boldbug9@gmail.com>
2026-06-09 19:58:48 +0530

    feature : add edit container to edit existing books using GUI

--- public/script.js
+++ public/script.js
@@ -1,56 +1,109 @@
-const InputContainer = document.getElementById("inpt-container");
-const bookContainer = document.getElementById("books-container");
+const InputContainer = document.getElementById('inpt-container')
+const bookContainer = document.getElementById('books-container')
+const editContainer = document.getElementById('edit-form')
+const editTitleInput = document.getElementById('edit-title-inpt')
+const editAuthorInput = document.getElementById('edit-author-inpt')
 
-    async function loadBooks(){
-        const response = await fetch("/books");
-        const books = await response.json();
-        bookContainer.innerHTML = "";
-        books.forEach(book => {
-            const bookHTML = `
+let localBooksArray = [];
+let currentEditingId = null;
+editContainer.style.display = 'none'
+
+async function loadBooks () {
+  const response = await fetch('/books')
+  const books = await response.json();
+  localBooksArray = books;
+  bookContainer.innerHTML = ''
+  books.forEach(book => {
+    const bookHTML = `
             <div class="book">
                 <h1>${book.title}</h1>
                 <p>${book.author}</p>
                 <button onClick = "deleteBook(${book.id})">Delete</button>
                 <button onClick = "editBook(${book.id})">Edit</button>
                 </div>
-                `;
-                
-                bookContainer.insertAdjacentHTML("beforeend",bookHTML);
-            });
-        }
-        
-        async function createBook(title, author) {
-            await fetch("/books", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    title,
-                    author
-                })
-            });
-            
-            loadBooks();
-        }
+                `
 
-        async function deleteBook(id) {
-            await fetch(`/books/${id}`, {
-                method: "DELETE"
-            });
+    bookContainer.insertAdjacentHTML('beforeend', bookHTML)
+  })
+}
 
-        loadBooks();
-        }
-        
-        InputContainer.addEventListener("submit",async (e)=>{
-            e.preventDefault();
+async function createBook (title, author) {
+  await fetch('/books', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title,
+      author
+    })
+  })
+
+  loadBooks()
+}
+
+async function deleteBook (id) {
+  await fetch(`/books/${id}`, {
+    method: 'DELETE'
+  })
+
+  loadBooks()
+}
+
+async function updateBook(id,title,author) {
+    await fetch(`/books/${id}`,{
+        method : "PATCH",
+        headers : {"Content-type" : "application/json"},
+        body : JSON.stringify({title,author}),
+    });
+
+    loadBooks();
     
-            const bookTitle = document.getElementById("title-inpt").value;
-            const authorName = document.getElementById("author-inpt").value;
-    
-            await createBook(bookTitle,authorName);
+}
 
-            InputContainer.reset();
-        });
+InputContainer.addEventListener('submit', async e => {
+  e.preventDefault()
 
-        loadBooks();
+  const bookTitle = document.getElementById('title-inpt').value
+  const authorName = document.getElementById('author-inpt').value
+
+  await createBook(bookTitle, authorName)
+
+  InputContainer.reset()
+})
+
+loadBooks()
+
+
+function hideEditContainer () {
+  editContainer.style.display = 'none';
+  currentEditingId = null;
+}
+
+function showEditContainer () {
+  editContainer.style.display = 'block'
+}
+
+function editBook (id) {
+  showEditContainer();
+  currentEditingId = id;
+  const book = localBooksArray.find(book => book.id === id);
+
+  if(book) {
+        // Populate the popup form with the book's current data
+        editTitleInput.value = book.title;
+        editAuthorInput.value = book.author;
+    }
+}
+
+
+editContainer.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+
+    const updatedTitle = editTitleInput.value;
+    const updatedAuthor = editAuthorInput.value;
+
+    await updateBook(currentEditingId,updatedTitle,updatedAuthor);
+
+    hideEditContainer();
+})
